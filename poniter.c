8fa34d3e992aa120ef771ad9283df46c49c42804
ThaiThanhDuy <just.electric.4.fun@gmail.com>
2024-03-13 10:20:02 +0700

    update file 13/3/24

--- poniter.c
+++ poniter.c
@@ -5,13 +5,17 @@ int i=0;
 void main(){
     int x= 10;
     int y =20;
-    int *px = &x;
-   
-    *px=20;
+    int *px = &x; // cu phap khai bao con tro con tro = dia chi bien
+    *px=20; // thay doi  gia tri con tro -> doi dia chi nho cua bien va gia tri cua bien
+    printf("%d\n",*px);
+    printf("%d\n",x);
+
     printf("%p\n",px);
     printf("%p\n",&x);
+
     printf("%p\n",*px);
     printf("%p\n",x);
+
     printf("%p\n",&y);
     printf("%p",y);
 
