653667b45e2aedd3e76cc8e83c53b05618e809ff
ThaiThanhDuy <just.electric.4.fun@gmail.com>
2024-04-19 12:21:52 +0700

    test linux

--- test.c
+++ test.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-void checkPalindrome(char* inputString)
-{
-  
-  for (int  i = 0; i < strlen(inputString); i++)
-  {
-    printf("%s",inputString[i]);  
-  }
-  
-}
+
 void main(){
-  
+  printf("hello");
 }
